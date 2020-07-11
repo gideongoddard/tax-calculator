cb599f14d9e91202b7d3d9b041dbf24fb26f9ecc
Gideon Goddard <goddard.gcw@gmail.com>
2020-07-11 20:17:37 +0100

    Updates

---
+++ precache-manifest.2e38bedbf0c7560e2173fa522f5bfb56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ebb989a960f8a3121b4ad3ba464d9b",
+    "url": "/tax-calculator/index.html"
+  },
+  {
+    "revision": "f925a0e8ea17482fea34",
+    "url": "/tax-calculator/static/css/main.4f19e7c6.chunk.css"
+  },
+  {
+    "revision": "914692be81f240205a26",
+    "url": "/tax-calculator/static/js/2.a043ef0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tax-calculator/static/js/2.a043ef0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f925a0e8ea17482fea34",
+    "url": "/tax-calculator/static/js/main.7d6a98bd.chunk.js"
+  },
+  {
+    "revision": "d04e2a747da3160f2a09",
+    "url": "/tax-calculator/static/js/runtime-main.4d80dbd4.js"
+  }
+]);
