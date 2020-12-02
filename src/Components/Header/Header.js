8f9690c8a00bc910b760e90640225ee719a526fd
Gideon Goddard <goddard.gcw@gmail.com>
2020-12-02 12:13:24 +0000

    Added icon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import Icon from "./tax-calc-icon.png"
 
 class Header extends React.Component {
     render() {
         return (
             <header>
                 <h1>{this.props.text}</h1>
+                <img src={Icon} alt="Pound sign icon" width="100%"/>
             </header>
         )
     }
